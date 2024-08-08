ec42fa1209d7e6fd458d4b2feabf000222a77192
Sonu <Sonukr@users.noreply.github.com>
2024-08-08 21:29:56 +0530

    added room updates for typing improved joining flow

--- chat-backend/room.js
+++ chat-backend/room.js
@@ -7,55 +7,73 @@ function createRoom(roomId) {
   return roomId;
 }
 
-function joinRoom(roomId, ws, userId) {
+function joinRoom(roomId, ws, userId, userName) {
   rooms[roomId].users.add(ws);
-  rooms[roomId].userIds[userId] = ws; // Map userId to websocket connection
+  rooms[roomId].userIds[userId] = { ws, userName }; // Map userId to websocket connection
 }
-
 
 function leaveRoom(roomId, ws, userId) {
   // Broadcast message about user leaving (optional)
-  const leaveMessage ={ type: 'userLeft', roomId, user: userId };
+  const leaveMessage = { type: "userLeft", roomId, user: userId };
   broadcastToRoom(roomId, leaveMessage);
 
   rooms[roomId].users.delete(ws);
   rooms[roomId].typingUsers.delete(ws);
   delete rooms[roomId].userIds[userId];
-  
+
   // Optionally handle cases where a room becomes empty (remove it)
-  
 }
 
-function startTyping(roomId, ws) {
+function startTyping(roomId, ws, userId) {
   rooms[roomId].typingUsers.add(ws);
-  broadcastTyping(roomId); // Broadcast typing indicator to all users
+  broadcastTyping(roomId, userId); // Broadcast typing indicator to all users
 }
 
-function stopTyping(roomId, ws) {
+function stopTyping(roomId, ws, userId) {
   rooms[roomId].typingUsers.delete(ws);
-  broadcastTyping(roomId); // Broadcast typing indicator update
+  broadcastTyping(roomId, userId); // Broadcast typing indicator update
 }
 
-function broadcastTyping(roomId) {
+function broadcastTyping(roomId, userId) {
   const isSomeoneTyping = rooms[roomId].typingUsers.size > 0;
-  const message = JSON.stringify({ type: 'typing', isSomeoneTyping });
+  const message = JSON.stringify({
+    type: isSomeoneTyping ? "typing" : "typingStopped",
+    isSomeoneTyping,
+    data: { user: userId },
+  });
   for (const client of rooms[roomId].users) {
     client.send(message);
   }
 }
+
 function broadcastToRoom(roomId, message) {
-  console.log(message)
+  console.log("broadcastToRoom", message);
   for (const client of rooms[roomId].users) {
-    client.send(JSON.stringify({ type: message.type || 'message', data: { message: message.message, user: message.user, timestamp: new Date()} }));
+    client.send(
+      JSON.stringify({
+        type: message.type || "message",
+        data: {
+          message: message.message,
+          user: message.user,
+          userName: message.userName,
+          timestamp: new Date(),
+        },
+      }),
+    );
   }
 }
 
 function getRoomUsers(roomId) {
   // Return an array of userIds in the room (consider privacy concerns)
-  return Object.keys(rooms[roomId].userIds);
-  
+  const userIds = Object.keys(rooms[roomId].userIds);
+  const userDetails = userIds.map((item) => {
+    return {
+      userId: item,
+      userName: rooms[roomId].userIds[item]["userName"],
+    };
+  });
+  return userDetails;
 }
-
 
 module.exports = {
   rooms,
--- chat-backend/websocket-server.js
+++ chat-backend/websocket-server.js
@@ -1,56 +1,89 @@
-const WebSocket = require('ws');
+const WebSocket = require("ws");
 
-const { rooms, joinRoom, createRoom, leaveRoom, startTyping,stopTyping, broadcastToRoom, getRoomUsers } = require('./room');
+const {
+  rooms,
+  joinRoom,
+  createRoom,
+  leaveRoom,
+  startTyping,
+  stopTyping,
+  broadcastToRoom,
+  getRoomUsers,
+} = require("./room");
 
 function startWebSocketServer(server, port) {
   const wss = new WebSocket.Server({ server });
-  
-  wss.on('connection', (ws) => {
-    console.log('Client connected');
 
-    ws.on('message', (message) => {
+  wss.on("connection", (ws) => {
+    console.log("Client connected");
+
+    ws.on("message", (message) => {
       const data = JSON.parse(message); // Assume JSON data format
-      if (data.type === 'join') {
+      if (data.type === "join") {
         const roomId = createRoom(data.roomId || uuid.v4());
         const userId = data.userId; // Assuming userId is sent along with the join command
-        joinRoom(roomId, ws, userId);
+        const userName = data.userName;
+        joinRoom(roomId, ws, userId, userName);
         console.log(`Client (user ${userId}) joined room: ${roomId}`);
-        ws.send(JSON.stringify({ type: 'joined', data:{ roomId, users: getRoomUsers(roomId)} })); // Send confirmation and current users
-        const messageData = { type: 'newUser', message: `${userId} has joined.`, user: userId }; // User data can be sent here
+        ws.send(
+          JSON.stringify({
+            type: "joined",
+            data: { roomId, users: getRoomUsers(roomId) },
+          }),
+        ); // Send confirmation and current users
+        const messageData = {
+          type: "newUser",
+          message: `${userId} has joined.`,
+          user: userId,
+          userName: userName,
+        }; // User data can be sent here
         broadcastToRoom(roomId, messageData);
-      } else if (data.type === 'startTyping') {
+      } else if (data.type === "startTyping") {
         const roomId = data.roomId;
+        const userId = data.userId;
         if (rooms[roomId]) {
-          startTyping(roomId, ws);
+          startTyping(roomId, ws, userId);
         } else {
-          console.warn(`Client tried to start typing in non-existent room: ${roomId}`);
+          console.warn(
+            `Client tried to start typing in non-existent room: ${roomId}`,
+          );
         }
-      } else if (data.type === 'stopTyping') {
+      } else if (data.type === "stopTyping") {
         const roomId = data.roomId;
+        const userId = data.userId;
         if (rooms[roomId]) {
-          stopTyping(roomId, ws);
+          stopTyping(roomId, ws, userId);
         } else {
-          console.warn(`Client tried to stop typing in non-existent room: ${roomId}`);
+          console.warn(
+            `Client tried to stop typing in non-existent room: ${roomId}`,
+          );
         }
-      } else if (data.type === 'message') {
+      } else if (data.type === "message") {
         const roomId = data.roomId;
         if (rooms[roomId]) {
           const messageData = { message: data.message, user: data.userId }; // User data can be sent here
           broadcastToRoom(roomId, messageData);
         } else {
-          console.warn(`Client tried to send message to non-existent room: ${roomId}`);
+          console.warn(
+            `Client tried to send message to non-existent room: ${roomId}`,
+          );
           // Optionally send an error message back to the client
-          ws.send(JSON.stringify({ message: `No room found for the roomId: ${roomId}` }))
+          ws.send(
+            JSON.stringify({
+              message: `No room found for the roomId: ${roomId}`,
+            }),
+          );
         }
       } else {
         console.warn(`Invalid message type: ${data.type}`);
       }
     });
 
-    ws.on('close', () => {
-      console.log('Client disconnected', ws);
+    ws.on("close", () => {
+      console.log("Client disconnected", ws);
       // Remove client from any room they were in (implement leaveRoom logic here)
-      const userId = ''/* Get user ID associated with the connection (from userIds map) */;
+      const userId =
+        ""; /* Get user ID associated with the connection (from userIds map) */
       // for (const roomId in rooms) {
       //   if (rooms[roomId].userIds[userId]) {
       //     leaveRoom(roomId, rooms[roomId].userIds[userId], userId);
